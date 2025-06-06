716ebcc4745208a62d9a1a8e887f25d0a4072530
Subin C Babu <163245107+SUBINCBABU@users.noreply.github.com>
2025-06-06 15:57:11 +0530

    Update api.service.ts

--- front end/src/app/api.service.ts
+++ front end/src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  baseurl = "http://localhost:3000/";
+  baseurl = "https://todo-list-angular.onrender.com/";
 
   constructor(private client: HttpClient) { }
 
